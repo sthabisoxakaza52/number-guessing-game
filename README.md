23aaae7d31cd984c8dffd4fb11b176afe6670a50
Sthabiso Alondwe Xakaza <sthabisoxakaza52@gmail.com>
2026-03-29 12:19:07 +0200

    Initial commit - number guessing game

---
+++ README.md
@@ -0,0 +1,32 @@
+## Number Guessing Game 
+
+## Description
+A simple Python game where the computer picks a random number between 1 and 100, and the player has to guess it. After each guess, the game gives feedback: 
+"Too high"
+"Too low"
+"Correct!"
+
+This project demonstrates Python basics such as functions, loops, conditionals, random numbers.
+
+## How to play
+1. Run the game using Python:
+python number_guess.py
+
+2. The computer will pick a random number between 1 and 100.
+
+3. Enter your guesses when prompted.
+
+4. Keep guessing until you find the correct number.
+
+5. Feedback will be displayed after each guess.
+
+## Features
+1. Random number generation between 1 and 100.
+
+2. Feedback for each guess: too high, too low  or correct
+
+##  Author 
+Sthabiso Alondwe Xakaza
+
+
+
---
+++ number_guess.py
@@ -0,0 +1,67 @@
+import random
+
+def number_guessing_game():
+    """
+    Play a number guessing game.
+
+    The computer picks a random number between 1 and 100. 
+    The player keeps guessing until they find the correct number.
+    After each guess, the function prints feedback:
+        - "Too high!" if the guess is larger than the target
+        - "Too low!" if the guess is smaller than the target
+        - "Correct!" if the guess matches the target
+    """
+    secret_number = random.randint(1,100)
+
+    print("  Welcome to the Number Guessing Game!  ")
+
+    user_response = input("Do you want to play the game? (yes/no): ")
+    user_response = user_response.lower()
+
+    if user_response != "yes":
+        print("Okay , goodbye!\n")
+        exit()
+
+    guess_count = 0
+
+    while True:
+        user_input = input("Enter your guess (1 - 100)  or 'quit' to exit: ")
+
+        if user_input.lower() == "quit":
+            print(f"\nGame exited. The number was {secret_number}.")
+            break
+
+        try:
+             user_guess = int(user_input)
+        except ValueError:
+             print("Enter a valid number or 'quit': ")
+             continue
+
+        guess_count += 1
+        max_guesses = 5
+    
+        if user_guess > secret_number:
+            print("Too High! Try Again.\n")
+        elif user_guess < secret_number:
+                print("Too Low! Try Again.\n")
+        else:
+            print("Correct!\n")
+            break
+
+        if guess_count == max_guesses:
+             print(f"You ran out of guesses. The number was {secret_number}.")
+             break
+       
+    print(f"You guessed the number in {guess_count} tries!\n")
+number_guessing_game()            
+
+
+
+
+
+
+
+
+
+
+
